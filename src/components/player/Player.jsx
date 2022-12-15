b98ef67cab8bf5fd977b6b188efae7306b2a1559
Elazar Durlacher <elazar91@gmail.com>
2022-12-15 12:23:31 +0200

    table

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Stats from "../stats/Stats";
 
-const Player = ({ player1 }) => {
-  console.log(player1);
+const Player = ({ players }) => {
+  console.log(players);
   return (
-    <table>
-      <tr></tr>
-      <tr>
-        <Stats player1={player1} />
-      </tr>
+    <table cellSpacing="0" border="1">
+      <tbody>
+        <tr>
+          <th>name</th>
+          <th>GP</th>
+          <th>MIN</th>
+          <th>PTS</th>
+          <th>FGM</th>
+          <th>FGA</th>
+          <th>FG%</th>
+          <th>3PM</th>
+          <th>3PA</th>
+          <th>3P%</th>
+          <th>FTM</th>
+          <th>FTA</th>
+          <th>FT%</th>
+          <th>OREB</th>
+          <th>DREB</th>
+          <th>REB</th>
+          <th>AST</th>
+          <th>STL</th>
+          <th>BLK</th>
+        </tr>
+        {players.length >= 0
+          ? players.map((player) => <Stats key={player?.id} player={player} />)
+          : null}
+      </tbody>
     </table>
   );
 };
--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import Stat from "../stat/Stat";
+import style from "./Stats.modules.scss";
 
-const Stats = ({ player1 }) => {
+const Stats = ({ player }) => {
   return (
     <>
       <tr>
-        <td>name</td>
-        <td>points</td>
-        <td>rebounds</td>
-        <td>assist</td>
-      </tr>
-      <tr>
         <td>
-          {player1?.playerDetails?.first_name}{" "}
-          {player1?.playerDetails?.last_name}
+          {player?.playerDetails?.first_name} {player?.playerDetails?.last_name}
         </td>
-        <Stat number={player1.stats?.pts} />
-        <Stat number={player1.stats?.reb} />
-        <Stat number={player1.stats?.ast} />
+        <Stat number={player?.stats?.games_played} />
+        <Stat number={player?.stats?.min} />
+        <Stat number={player?.stats?.pts} />
+        <Stat number={player?.stats?.fgm} />
+        <Stat number={player?.stats?.fga} />
+        <Stat number={player?.stats?.fg_pct} />
+        <Stat number={player?.stats?.fg3m} />
+        <Stat number={player?.stats?.fg3a} />
+        <Stat number={player?.stats?.fg3_pct} />
+        <Stat number={player?.stats?.ftm} />
+        <Stat number={player?.stats?.fta} />
+        <Stat number={player?.stats?.ft_pct} />
+        <Stat number={player?.stats?.oreb} />
+        <Stat number={player?.stats?.dreb} />
+        <Stat number={player?.stats?.reb} />
+        <Stat number={player?.stats?.ast} />
+        <Stat number={player?.stats?.stl} />
+        <Stat number={player?.stats?.blk} />
       </tr>
     </>
   );
