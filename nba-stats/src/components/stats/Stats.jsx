7f2f72679e0fcdfd1a63f8faedf67f708833326a
Elazar Durlacher <elazar91@gmail.com>
2023-04-14 02:47:51 +0300

    add getMaxValue func to find the highest stat in every category

--- nba-stats/src/components/stats/Stats.jsx
+++ nba-stats/src/components/stats/Stats.jsx
@@ -1,18 +1,61 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Stat from "../stat/Stat";
-import style from "./Stats.module.scss";
+import Style from "./Stats.module.scss";
 
 const Stats = ({ player, players }) => {
-  const [highest, setHighest] = useState(false);
+  const [highest, setHighest] = useState({
+    games_played: null,
+    player_id: null,
+    season: null,
+    min: null,
+    fgm: null,
+    fga: null,
+    fg3m: null,
+    fg3a: null,
+    ftm: null,
+    fta: null,
+    oreb: null,
+    dreb: null,
+    reb: null,
+    ast: null,
+    stl: null,
+    blk: null,
+    turnover: null,
+    pf: null,
+    pts: null,
+    fg_pct: null,
+    fg3_pct: null,
+    ft_pct: null,
+  });
 
   useEffect(() => {
-    players.sort((player) => player);
-  }, []);
+    // console.log(Math.max(...players.map((player) => player?.stats.pts)));
+    // console.log(players.map((player) => Object.keys(player.stats)));
+    getMaxValues(players);
+    // console.log(highest);
+  }, [players]);
+
+  const getMaxValues = (arr) => {
+    const result = arr.reduce((result, obj) => {
+      let object = obj.stats;
+      for (let key in object) {
+        if (
+          (typeof object[key] === "number" && result[key] === undefined) ||
+          object[key] > result[key]
+        ) {
+          result[key] = object[key];
+        }
+      }
+      return result;
+    }, {});
+    setHighest(result);
+    console.log(highest);
+  };
 
   return (
     <tr>
-      <td>
+      <td className={Style.firstName}>
         {player?.playerDetails?.first_name} {player?.playerDetails?.last_name}
       </td>
       <Stat number={player?.stats?.games_played} highest={highest} />
